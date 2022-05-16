944407ae0996f39476ad0be1604f5ce6eab1f78f
MoneshROR <monesh.thoughtwin@gmail.com>
2022-05-16 15:59:12 +0530

    first typescript program

---
+++ first.js
@@ -0,0 +1,13 @@
+var message = "hello typescript";
+console.log(message);
+var name1 = "indore";
+console.log(name1);
+var user = {
+    city: "Delhi",
+    domain: "Information Technology"
+};
+console.log(user.city);
+console.log(user.domain);
+var lower_case = "HELLO MANIA";
+var m = lower_case.toLocaleLowerCase();
+console.log(m);
---
+++ first.ts
@@ -0,0 +1,15 @@
+const message = "hello typescript";
+console.log(message);
+var name1 = "indore";
+console.log(name1);
+
+const user = {
+    city: "Delhi",
+    domain: "Information Technology"
+};
+console.log(user.city);
+console.log(user.domain);
+
+let lower_case = "HELLO MANIA";
+let m=lower_case.toLocaleLowerCase();
+console.log(m);
